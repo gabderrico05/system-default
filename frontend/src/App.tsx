1c279f2fcae2312daa8706f865499dde03fad30c
Gabriel Derrico <derrico.g@aluno.ifsp.edu.br>
2026-03-24 22:14:34 -0300

    initial commit: install React, Vite and ESLint with a basic configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+
+
+  return (
+   <div className='w-full h-full flex items-center justify-center bg-stone-500'> 
+      <h2 className='text-stone-700 font-bold text-5xl'>Sistema Default</h2>
+   </div>
+  )
+}
+
+export default App
